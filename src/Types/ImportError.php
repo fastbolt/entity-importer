df1662a45d8fa282255232027e1c8c9291302974
Daniel Hirtzbruch <dhh@fastbolt.com>
2023-02-10 10:30:00 +0100

    fix

--- src/Types/ImportError.php
+++ src/Types/ImportError.php
@@ -8,41 +8,18 @@
 
 namespace Fastbolt\EntityImporter\Types;
 
-class ImportError
+use Exception;
+
+class ImportError extends Exception
 {
-    /**
-     * @var int
-     */
-    private int $line;
-
-    /**
-     * @var string
-     */
-    private string $message;
-
     /**
      * @param int    $line
      * @param string $message
      */
     public function __construct(int $line, string $message)
     {
-        $this->line    = $line;
-        $this->message = $message;
-    }
+        $this->line = $line;
 
-    /**
-     * @return int
-     */
-    public function getLine(): int
-    {
-        return $this->line;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMessage(): string
-    {
-        return $this->message;
+        parent::__construct($message);
     }
 }
